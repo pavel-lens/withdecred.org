bee08fd7024393a012a07d7d79dbbc39bfef53a5
Pavel Svitek <pavel.svitek@maklando.ch>
2020-06-07 11:44:47 +0200

    implement StakeyCard

---
+++ components/StakeyCard/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="c-stakey-card">
+    <el-popover
+      v-model="isVisible"
+      placement="top"
+      title="Did you know?"
+      trigger="manual"
+      offset="-50"
+      width="300"
+    >
+      <!-- <el-button slot="reference" @click="visible = !visible"
+        >Manual to activate</el-button
+      > -->
+      <!-- <span> -->
+      <img
+        slot="reference"
+        @click="isVisible = !isVisible"
+        src="./stakey-thumbsup.png"
+        width="auto"
+        height="150"
+      />
+
+      <div>
+        We also have a Lightning network!
+        <div class="text--align-right mt-3">
+          <el-button type="primary" size="mini" plain>Read more</el-button>
+        </div>
+      </div>
+    </el-popover>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      isVisible: false,
+    }
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.isVisible = true
+    }, 500)
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.c-stakey-card {
+  display: block;
+}
+</style>
