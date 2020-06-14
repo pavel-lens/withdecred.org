ac6326c9f443b69529a12c25a60171c01348a776
Pavel Svitek <pavel.svitek@maklando.ch>
2020-06-14 15:55:46 +0200

    configure nuxtjs-countly module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   mode: 'universal',
   /*
@@ -46,7 +48,25 @@ module.exports = {
   modules: [
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
+    '@marcdiethelm/nuxtjs-countly',
   ],
+  /*
+   ** Countly tracker configuration
+   */
+  countly: {
+    // Required, the URL of your Countly server
+    url: 'https://analytics.withdecred.org',
+    // Required, copy from server's management > apps page
+    app_key: process.env.ANALYTICS_KEY,
+    // For self-hosting... use original .js or .min.js (minified)
+    trackerSrc: '/countly.min.js',
+    // Automatic tracking, if not array this defaults to ['track_sessions', 'track_pageview', 'track_links']
+    trackers: null,
+    // Append a <noscript> tag with tracking pixel <img> to <body>, default: true
+    noScript: true,
+    // Log Countly debug info to console, default: false
+    debug: process.env.NODE_ENV !== 'production',
+  },
   /*
    ** Build configuration
    */
