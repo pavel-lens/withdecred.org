3daa3753d119ab046274defb595e9c27eb5c5530
Pavel Svitek <pavel.svitek@maklando.ch>
2020-09-15 10:07:47 +0200

    server/index - enforce SSL

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 // Required for 'typedi' module
 import 'reflect-metadata'
-const express = require('express')
+import express, { Request, Response, NextFunction } from 'express'
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
@@ -17,6 +17,13 @@ import routes from './routes'
 
 // Set console as Logger
 Container.set('Logger', console)
+
+function forceSsl(req: Request, res: Response, next: NextFunction) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://withdecred.org', req.url].join(''))
+  }
+  return next()
+}
 
 async function start() {
   // Init Nuxt.js
@@ -35,11 +42,16 @@ async function start() {
   // Setup routes
   app.use('/api', routes)
 
+  // Enforce SSL in production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(forceSsl)
+  }
+
   // Give nuxt middleware to express
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(Number(port), host, () => {})
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true,
